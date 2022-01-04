2cc2d4c9512231fbdd8a25a594e585a4dbba5f35
Shisus <jesus.laime@camba.coop>
2022-01-04 10:30:26 -0300

    add handle deploy user proxy, creates does not exists

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,3 +1,21 @@
-import {DeployProxy} from "../generated/PRBProxyFactory/PRBProxyFactory";
+import { Bytes } from "@graphprotocol/graph-ts";
+import { DeployProxy } from "../generated/PRBProxyFactory/PRBProxyFactory";
+import { UserProxy } from "../generated/schema";
 
-export function handleDeployProxy(event: DeployProxy): void {}
+export function handleDeployProxy(event: DeployProxy): void {
+  let userAddress = event.params.owner;
+  let proxyAddress = event.params.proxy;
+  createUserProxyIfNonExistent(userAddress, proxyAddress);
+}
+
+export function createUserProxyIfNonExistent(userAddress: Bytes, proxyAddress: Bytes): UserProxy {
+  let address = userAddress.toHexString();
+  let userProxy = UserProxy.load(address);
+  if (userProxy == null) {
+    userProxy = new UserProxy(address);
+  }
+  userProxy.userAddress = userAddress;
+  userProxy.proxyAddress = proxyAddress;
+  userProxy.save();
+  return userProxy as UserProxy;
+}
