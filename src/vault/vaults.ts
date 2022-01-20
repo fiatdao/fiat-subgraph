869b59f62c2574a6a4cb8694a6b4f496dbf9012c
Shisus <jesus.laime@camba.coop>
2022-01-20 15:39:07 -0300

    feat: adds create collateral when a new vault is created

--- src/vault/vaults.ts
+++ src/vault/vaults.ts
@@ -1,26 +1,24 @@
-import { Bytes } from "@graphprotocol/graph-ts";
+import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { Init } from "../../generated/Codex/Codex";
 import { Vault } from "../../generated/schema";
+import { createCollateralIfNecessary } from "../collaterals";
 import { vaultsData } from "./vaultsData";
 
 export function handleVaultInit(event: Init): void {
   let vaultAddress = event.params.vault;
-  createVaultIfNonExistent(vaultAddress);
+  createVaultIfNonExistent(vaultAddress.toHexString());
 }
 
-export function createVaultIfNonExistent(vaultAddress: Bytes): Vault {
-  let address = vaultAddress.toHexString();
-  let vault = Vault.load(address);
+export function createVaultIfNonExistent(vaultAddress: string): Vault {
+  let vault = Vault.load(vaultAddress);
   if (vault == null) {
-    vault = new Vault(address);
-    vault.address = vaultAddress;
+    vault = new Vault(vaultAddress);
+    vault.address = Address.fromString(vaultAddress);
 
-    let config = vaultsData.get(address);
+    let config = vaultsData.get(vaultAddress);
     if (config) {
       vault.name = (config.get('name')) as string;
-      vault.underlyingAsset = (config.get('underlyingAsset')) as string;
-      vault.originator = (config.get('originator'))  as string;
-      vault.type = (config.get('type'))  as string;
+      createCollateralIfNecessary(vault!, config!)
     }
     vault.save();
   }
