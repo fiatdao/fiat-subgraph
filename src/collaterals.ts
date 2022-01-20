955e43c3c420afefb2b70e6caecff7b7bfb584b8
Shisus <jesus.laime@camba.coop>
2022-01-20 15:38:23 -0300

    feat: add collateral creation handler

---
+++ src/collaterals.ts
@@ -0,0 +1,62 @@
+import { Address, BigInt, Bytes, TypedMap } from "@graphprotocol/graph-ts";
+import { Notional } from "../generated/Notional/Notional";
+import { ERC20 } from "../generated/Notional/ERC20";
+import { Collateral, Vault } from "../generated/schema";
+import { createVaultIfNonExistent } from "./vault/vaults";
+import { VAULT_NOTIONAL_ADDRESS } from "./constants";
+
+const ELEMENT = "Element";
+
+export function createCollateralIfNecessary(vault: Vault, config: TypedMap<string, string>): void {
+  if (vault.name === ELEMENT) {
+    let underlierToken = (config.get('underlierToken')) as string;
+    let collateral = Collateral.load(underlierToken!);
+
+    if (!collateral) {
+      collateral = new Collateral(underlierToken!);
+      collateral.name = (config.get('name')) as string;
+      collateral.originator = (config.get('originator')) as string;
+      collateral.type = (config.get('type')) as string;
+      let maturity = config.get('maturity') as string;
+      if (maturity !== "") {
+        collateral.maturity = BigInt.fromString(maturity);
+      }
+      collateral.underlierName = (config.get('underlierName')) as string;
+      collateral.underlierAddress = Address.fromString(config.get('underlierAddress') as string);
+      collateral.vault = vault.id;
+      collateral.save();
+    }
+  }
+}
+
+export function createNotionalCollateralIfNonExistent(notional: Notional, currencyId: i32, maturity: BigInt): Collateral {
+  let id = currencyId.toString() + "-" + maturity.toString();
+  let collateral = Collateral.load(id);
+  if (!collateral) {
+    let collateral = new Collateral(id);
+    let currency = notional.getCurrency(currencyId);
+    let tokenAddress = currency.value0.tokenAddress;
+    let underlierAddress = currency.value1.tokenAddress;
+    let tokenAsset = ERC20.bind(tokenAddress);
+    let tokenName = tokenAsset.name();
+    let collateralName = tokenName + "-" + maturity.toString();
+
+    collateral.address = tokenAddress;
+    collateral.name = collateralName;
+    collateral.maturity = maturity;
+    if (underlierAddress !== null) {
+      collateral.underlierAddress = underlierAddress;
+      let underlierAsset = ERC20.bind(underlierAddress);
+      let underlierName = underlierAsset.try_name()
+      if (!underlierName.reverted) {
+        collateral.underlierName = underlierName.value;
+      }
+    }
+    collateral.originator = tokenName;
+    collateral.type = tokenAsset.symbol();
+    createVaultIfNonExistent(VAULT_NOTIONAL_ADDRESS);
+    collateral.vault = VAULT_NOTIONAL_ADDRESS;
+    collateral.save();
+  }
+  return collateral as Collateral;
+}
