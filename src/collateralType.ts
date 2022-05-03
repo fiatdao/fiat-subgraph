d0193c92b63274acc20d1447a171cc6776ea1ef2
SolcAndMe <97065474+SolcAndMe@users.noreply.github.com>
2022-05-03 22:33:22 +0100

    Add Notional Finance config for goerli

--- src/collateralType.ts
+++ src/collateralType.ts
@@ -1,17 +1,12 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 import { CollateralType, Vault } from "../generated/schema";
-import { IVault as IVaultContract } from "../generated/Codex/IVault";
 import { ERC20 as ERC20Contract } from "../generated/Codex/ERC20";
+import { IVault as IVaultContract } from "../generated/Codex/IVault";
 import { VAULT_CONFIG } from "./generated/config";
-import { createEPTDataIfNonExistent } from "./element";
-import { BIGINT_ZERO, WAD } from "./utils";
-// import { Notional, Notional__getCurrencyResult } from "../generated/Notional/Notional";
-// import { VaultFC } from "../generated/Notional/VaultFC";
-// import { createVaultIfNonExistent } from "./vault/vaults";
 
-// const NOTIONAL = "NOTIONAL";
-// const NOTIONAL_COLLATERAL_TYPE = "fCash";
-// const VAULT_TYPE_ERC20 = "ERC20";
+import { BIGINT_ZERO, WAD } from "./utils";
+import { createEPTDataIfNonExistent } from "./element";
+import { createFCDataIfNonExistent } from "./notional";
 
 export function createCollateralTypeIfNonExistent(vault: Vault, tokenId: BigInt): CollateralType {
   let id = vault.id + "-" + tokenId.toString();
@@ -44,7 +39,7 @@ export function createCollateralTypeIfNonExistent(vault: Vault, tokenId: BigInt)
     if (((config.get('type')) as string) == "ELEMENT") {
       createEPTCollateralTypeIfNonExistent(collateralType);
     } else if (((config.get('type')) as string) == "NOTIONAL") {
-      // createNotionalCollateralTypeIfNonExistent(vault, tokenId);
+      createFCCollateralTypeIfNonExistent(collateralType);
     }
   }
 
@@ -58,43 +53,9 @@ function createEPTCollateralTypeIfNonExistent(collateralType: CollateralType): C
   return collateralType as CollateralType;
 }
 
-// TODO
-// export function createNotionalCollateralTypeIfNonExistent(notional: Notional, tokenId: BigInt, currencyId: i32, maturity: BigInt, tenor: BigInt): CollateralType {
-//   let id = tokenId.toString();
-
-//   let collateralType: CollateralType;
-//   let vault: Vault;
-//   let currency: Notional__getCurrencyResult;
-
-//   // look for corresponding vault
-//   for (let i: i32 = 0; i < VAULT_CONFIG.entries.length; i++) {
-//     let vData = VAULT_CONFIG.get(VAULT_CONFIG.entries[i].key);
-
-//     // only notional vaults
-//     let vaultType = vData!.get('type');
-//     if (VAULT_CONFIG.entries[i].key !== "" && vaultType !== null && vaultType === NOTIONAL) {
-//       currency = notional.getCurrency(currencyId);
-//       let marketUnderlierAddress = currency.value1.tokenAddress;
-
-//       let vaultFC = VaultFC.bind(changetype<Address>(Address.fromHexString(VAULT_CONFIG.entries[i].key)))
-//       let vaultUnderlier = vaultFC.underlierToken()
-//       let vaultTenor = vaultFC.tenor()
-
-//       // same underlier and tenor
-//       if (vaultUnderlier.equals(marketUnderlierAddress) && vaultTenor.equals(tenor)) {
-//         vault = createVaultIfNonExistent(VAULT_CONFIG.entries[i].key);
-//         let collateralType = createCollateralTypeIfNonExistent(vault, id);
-//         if (!collateralType.address) {
-//           collateralType.maturity = maturity;
-//           collateralType.address = notional._address;
-//           collateralType.symbol = getSymbol(notional._address);
-//           collateralType.underlierAddress = marketUnderlierAddress;
-//           collateralType.underlierSymbol = getSymbol(marketUnderlierAddress);
-//           collateralType.save();
-//         }
-//       }
-//     }
-//   }
-
-//   return collateralType as CollateralType;
-// }
+function createFCCollateralTypeIfNonExistent(collateralType: CollateralType): CollateralType {
+  collateralType.fcData = createFCDataIfNonExistent(collateralType.vault!).id;
+  collateralType.save();
+  
+  return collateralType as CollateralType;
+}
--- src/notional.ts
+++ src/notional.ts
@@ -1,56 +1,20 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-// import { MarketsInitialized, Notional } from "../generated/Notional/Notional";
-// import { createNotionalCollateralTypeIfNonExistent } from "./collateralType";
+import { Address } from "@graphprotocol/graph-ts";
+import { FCData } from "../generated/schema";
+import { VaultFC } from "../generated/Codex/VaultFC";
+import { VAULT_CONFIG } from "./generated/config";
 
-export const RATE_PRECISION = 1000000000;
-export const BASIS_POINTS = 100000;
-export const MAX_TRADED_MARKET_INDEX = 7
-export const DAY = 86400;
-export const WEEK = DAY * 6;
-export const MONTH = DAY * 30;
-export const QUARTER = DAY * 90;
-export const YEAR = QUARTER * 4;
+export function createFCDataIfNonExistent(vaultAddress: string): FCData {
+  let fcData = FCData.load(vaultAddress);
+  if (fcData == null) {
+    fcData = new FCData(vaultAddress);
+    let config = VAULT_CONFIG.get(vaultAddress);
+    if (config && ((config.get('type')) as string) == "NOTIONAL") {
+      let vaultFC = VaultFC.bind(Address.fromString(vaultAddress));
+      fcData.notional = vaultFC.token(); // Notional Monolith
+      fcData.tenor = vaultFC.tenor();
+    }
 
-export function getTimeRef(timestamp: i32): i32 {
-  return timestamp - (timestamp % QUARTER);
-}
-
-function getTradedMarket(index: i32) : i32 {
-  if (index == 1) return QUARTER;
-  if (index == 2) return 2 * QUARTER;
-  if (index == 3) return YEAR;
-  if (index == 4) return 2 * YEAR;
-  if (index == 5) return 5 * YEAR;
-  if (index == 6) return 10 * YEAR;
-  if (index == 7) return 20 * YEAR;
-
-  return 0;
-}
-
-function getMarketTenor(maturity: BigInt, blockTime: BigInt): BigInt {
-  let tRef = BigInt.fromI32(getTimeRef(blockTime.toI32()));
-
-  for (let i = 1; i <= MAX_TRADED_MARKET_INDEX; i++) {
-      let tadeMarket = getTradedMarket(i);
-      let marketMaturity = tRef.plus(BigInt.fromI32(tadeMarket));
-      if (marketMaturity.equals(maturity)) return BigInt.fromI32(tadeMarket);
+    fcData.save();
   }
-
-  return BigInt.fromI32(0)
+  return fcData as FCData;
 }
-
-// export function handleMarketsInitialized(event: MarketsInitialized): void {
-//   let currencyId = event.params.currencyId;
-//   let tRef = getTimeRef(event.block.timestamp.toI32());
-//   let notional = Notional.bind(event.address);
-//   let marketsResult = notional.getActiveMarketsAtBlockTime(currencyId, BigInt.fromI32(tRef));
-
-//   for (let i: i32 = 0; i < marketsResult.length; i++) {
-//     let maturity = marketsResult[i].maturity;
-//     let tenor = getMarketTenor(maturity, event.block.timestamp);
-//     let assetType = 1;
-//     let tokenId = notional.encodeToId(currencyId, maturity, assetType);
-
-//     createNotionalCollateralTypeIfNonExistent(notional, tokenId, currencyId, maturity, tenor);
-//   }
-// }
